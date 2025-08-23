83ff79e5556f3bbb054c6746a3a19b87fa5087c9
mst <mst.naher.bd@gmail.com>
2025-08-23 22:13:20 +0100

    added toggling between two buttons add-money and cash-out-money

--- home.js
+++ home.js
@@ -1,17 +1,31 @@
 // console.log('home page is connected')
+
+
+const validPin = 1234
 document.getElementById("add-monet-btn").addEventListener('click', function(e){
 e.preventDefault();
 // console.log('add money btn clicked')
 const bank = document.getElementById("bank").value;
 const accountNumber = document.getElementById("account-number").value;
 const amount = parseInt(document.getElementById("add-amount").value);
-const pin = document.getElementById("add-pin").value;
+const pin = parseInt(document.getElementById("add-pin").value);
 // console.log(bank, accountNumber, amount, pin)
 
 const availableBalance = parseInt(
   document.getElementById("available-balance").innerText
 );
-// console.log(availableBalance)
+// console.log(amount, availableBalance)
+
+if(accountNumber.length <11){
+ alert('Please provide valid account Number')
+ return;
+}
+
+if (pin !== validPin) {
+  alert('Please provide valid pin Number')
+  return;
+}
+
 
 const totalNewAvailabeBalance = amount + availableBalance;
 
@@ -19,6 +33,17 @@ const totalNewAvailabeBalance = amount + availableBalance;
  document.getElementById("available-balance").innerText =
    totalNewAvailabeBalance;
 
+});
 
+// Toggling Feature:
 
+document.getElementById("add-button").addEventListener('click', function(){
+
+  document.getElementById("cash-out-parent").style.display = "none";
+  document.getElementById("add-money-parent").style.display = "block";
+});
+
+document.getElementById("cash-out-button").addEventListener('click', function(){
+  document.getElementById("add-money-parent").style.display = 'none'
+  document.getElementById("cash-out-parent").style.display = 'block';
 });
